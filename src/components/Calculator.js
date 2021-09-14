14268c5ebbfc0401f0aa38d00b153f9cd83f3a51
Amine Smahi <md.amine.smahi@gmail.com>
2021-09-14 19:56:56 +0100

    change the colors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,22 +13,22 @@ class Calculator extends React.Component {
         <span className="char">AC</span>
         <span className="char">+/-</span>
         <span className="char">%</span>
-        <span className="char">/</span>
+        <span className="char orange">/</span>
         <span className="char">7</span>
         <span className="char">8</span>
         <span className="char">9</span>
-        <span className="char">*</span>
+        <span className="char orange">*</span>
         <span className="char">4</span>
         <span className="char">5</span>
         <span className="char">6</span>
-        <span className="char">-</span>
+        <span className="char orange">-</span>
         <span className="char">1</span>
         <span className="char">2</span>
         <span className="char">3</span>
-        <span className="char">+</span>
+        <span className="char orange">+</span>
         <span className="zero">0</span>
         <span className="char">.</span>
-        <span className="char">=</span>
+        <span className="char orange">=</span>
       </div>
     );
   }
