a1084b60629640c776f9dbc55e63f0f1c3dff51a
Amine Smahi <md.amine.smahi@gmail.com>
2021-09-22 15:26:24 +0100

    Add new pages home and quotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Quote from './pages/Quote';
+import Header from './components/Header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,7 +13,30 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Header className="header" />
+        <Switch>
+          <Route exact path="/calculator">
+            <div className="calculator-page">
+              <h2>Let&lsquo;s do some Math</h2>
+              <Calculator />
+            </div>
+          </Route>
+          <Route path="/quotes">
+            <Quote />
+          </Route>
+          <Route path="/">
+            <div className="about">
+              <p>
+                &quot;Math magicians&quot; is a website for all fans of
+                mathematics. It is a Single Page App (SPA) that allows users to:
+                Make simple calculations. Read a random math-related quote.
+              </p>
+            </div>
+          </Route>
+          <Route path="*">
+            <h3>No match for this page</h3>
+          </Route>
+        </Switch>
       </div>
     );
   }
---
+++ src/pages/Quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Quote = () => (
+  <h1 className="Quote">
+    Mathematics is not about numbers, equations, computations, or algorithms:
+    it&lsquo;s about understanding.--WIlliam Paul Thurston
+  </h1>
+);
+
+export default Quote;
